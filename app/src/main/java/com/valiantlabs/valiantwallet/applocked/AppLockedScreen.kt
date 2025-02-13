7ca38d6cfc2b6ea2839b9cf9bc7b3e47ae425461
Arfin Hossin <developerarfin@gmail.com>
2025-02-13 21:01:31 +0600

    Initial Project

---
+++ app/src/main/java/com/valiantlabs/valiantwallet/applocked/AppLockedScreen.kt
@@ -0,0 +1,18 @@
+package com.valiantlabs.valiantwallet.applocked
+
+import android.content.Context
+
+class AppLockedScreen {
+}
+
+//private fun osAuthentication(
+//    context: Context,
+//    onShowOSBiometricsModal: () -> Unit,
+//    onContinueWithoutAuthentication: () -> Unit
+//) {
+//    if (hasLockScreen(context)) {
+//        onShowOSBiometricsModal()
+//    } else {
+//        onContinueWithoutAuthentication()
+//    }
+//}
